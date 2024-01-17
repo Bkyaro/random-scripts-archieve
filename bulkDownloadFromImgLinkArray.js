81d9a43b2814a22cc7f1e4f992472722bf1ed1b3
Jackal_lzt <bkyarokun@gmail.com>
2024-01-17 09:34:07 +0800

    feat: bulk download from an array of image url

---
+++ bulkDownloadFromImgLinkArray.js
@@ -0,0 +1,43 @@
+/**
+ * @param {*} imageUrl : {category: string, images: string[]}
+ * @param {*} categoryName
+ */
+function downloadImage(imageUrl, categoryName) {
+  fetch(imageUrl)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const timestamp = new Date().getTime();
+      const filename = `${categoryName}_${timestamp}.png`;
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(() => {
+        URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      }, 100); // 略微增加延迟时间以保证处理
+    })
+    .catch((error) => console.error("图片下载失败:", error));
+}
+
+function downloadImagesSequentially(imageData, index = 0) {
+  if (
+    !imageData ||
+    !Array.isArray(imageData.images) ||
+    imageData.images.length === 0
+  ) {
+    console.log("请输入包含图片链接的正确数据");
+    return;
+  }
+  if (index >= imageData.images.length) {
+    return; // 所有图片已处理完毕
+  }
+
+  const categoryName = imageData.category || "default";
+  downloadImage(imageData.images[index], categoryName);
+  setTimeout(() => {
+    downloadImagesSequentially(imageData, index + 1); // 递归调用以下载下一张图片
+  }, 500); // 增加间隔时间以避免并发下载
+}
